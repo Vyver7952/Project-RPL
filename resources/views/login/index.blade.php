022b590082051791e4476cadfb62f2e8eef40283
Vyver7952 <danielbudip789077@gmail.com>
2022-11-23 11:43:53 +0700

    Ganti Login View

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -2,91 +2,38 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
     <link rel="stylesheet" href="css/style.css" />
 </head>
 
 <body>
-    <section class="h-100 gradient-form" style="background-color: #eee">
-        <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-xl-10">
-
-                    @if (session()->has('loginError'))
-                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                            {{ session('loginError') }}
-                            <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                aria-label="Close"></button>
-                        </div>
-                    @endif
-                    @if (session()->has('logout'))
-                        <div class="alert alert-success alert-dismissible fade show" role="alert">
-                            {{ session('logout') }}
-                            <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                aria-label="Close"></button>
-                        </div>
-                    @endif
-
-                    <div class="card rounded-3 text-black">
-                        <div class="row g-0">
-                            <div class="col-lg-6">
-                                <div class="card-body p-md-5 mx-md-4">
-                                    <div class="text-center">
-                                        <img src="img/{{ $logo }}" style="width: 150px" alt="logo" />
-                                        <h4 class="mt-1 mb-5 pb-1">Koperasi Arta Pratama</h4>
-                                    </div>
-
-                                    <form action="/login" method="POST">
-                                        @csrf
-                                        <p>Please login to your account</p>
-
-                                        <div class="form-outline mb-4">
-                                            <input type="text" id="username" name="username"
-                                                class="form-control @error('username') is-invalid @enderror"
-                                                placeholder="Username" autofocus required />
-                                            <label class="form-label" for="username" name="username">Username</label>
-                                            @error('username')
-                                                <div class="invalid-feedback">
-                                                    {{ $message }}
-                                                </div>
-                                            @enderror
-                                        </div>
-
-                                        <div class="form-outline mb-4">
-                                            <input type="password" id="password" name="password"
-                                                class="form-control @error('password') is-invalid @enderror"
-                                                placeholder="Password" required />
-                                            <label class="form-label" for="password" name="password">Password</label>
-                                            @error('password')
-                                                <div class="invalid-feedback">
-                                                    {{ $message }}
-                                                </div>
-                                            @enderror
-                                        </div>
-
-                                        <div class="text-center pt-1 mb-5 pb-1">
-                                            <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
-                                                type="submit">
-                                                Login
-                                            </button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                                <div class="text-white px-3 py-4 p-md-5 mx-md-4"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <div class="wrapper">
+        <div class="logo">
+            <img src="img/{{ $logo }}" alt="logo" />
         </div>
-    </section>
+        <div class="text-center mt-4 name">
+            Koperasi Arta Pratama
+        </div>
+
+        <form action="/login" class="p-3 mt-3">
+            @csrf
+            <div class="form-field d-flex align-items-center">
+                <span class="far fa-user"></span>
+                <input type="text" name="userName" id="userName" placeholder="Username">
+            </div>
+            <div class="form-field d-flex align-items-center">
+                <span class="fas fa-key"></span>
+                <input type="password" name="password" id="pwd" placeholder="Password">
+            </div>
+            <button class="btn mt-3" type="submit">Login</button>
+        </form>
+    </div>
 </body>
 
 </html>
