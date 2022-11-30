d0990fd58eb67ef8e5cbcae654aff01ece3dfd41
Vyver7952 <danielbudip789077@gmail.com>
2022-11-30 20:13:08 +0700

    Cumulative Chart

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -12,7 +12,7 @@ class MainController extends Controller
 {
     public function home()
     {
-        $chartArea = Simpanan::select(DB::raw("SUM(saldo) as jumlah"), DB::raw("MONTHNAME(created_at) as month_name"))
+        $chartArea = Simpanan::select(DB::raw("SUM(SUM(saldo)) OVER (ORDER BY MONTH(created_at)) as jumlah"), DB::raw("MONTHNAME(created_at) as month_name"))
             ->whereYear('created_at', date('Y'))
             ->groupBy(DB::raw("Month(created_at)"))
             ->pluck('jumlah', 'month_name');
